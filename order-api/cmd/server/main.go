9eb9900ffbe1234e76af9babbd1bce9f2a462426
hivictorhugo <vhqueirozcouto@gmail.com>
2025-08-12 11:30:52 -0300

    first commit - creating folders

---
+++ order-api/cmd/server/main.go
@@ -0,0 +1,39 @@
+go
+package main
+
+import (
+    "database/sql"
+    "log"
+    "net/http"
+    "os"
+
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/username/affiliate-conversions/internal/db"
+    "github.com/username/affiliate-conversions/internal/handlers"
+    "github.com/username/affiliate-conversions/internal/middleware"
+)
+
+func main() {
+    dsn := os.Getenv("DB_DSN")
+    if dsn == "" {
+        log.Fatal("DB_DSN is required")
+    }
+
+    database, err := db.New(dsn)
+    if err != nil {
+        log.Fatalf("db connect: %v", err)
+    }
+    defer database.Close()
+
+    h := handlers.NewConversionHandler(database)
+
+    mux := http.NewServeMux()
+    mux.Handle("/conversions", middleware.Logging(middleware.LimitBody(http.HandlerFunc(h.HandleConversions), 1<<20)))
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    log.Printf("listening on :%s", port)
+    log.Fatal(http.ListenAndServe(":"+port, mux))
+}
