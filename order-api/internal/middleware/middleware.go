30a2aed889a2cfa2ba6913583cdc4d4db765d408
hivictorhugo <vhqueirozcouto@gmail.com>
2025-08-13 17:28:44 -0300

    add middleware.go

---
+++ order-api/internal/middleware/middleware.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+func Logging(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        next.ServeHTTP(w, r)
+        log.Printf("%s %s %s", r.Method, r.URL.Path, time.Since(start))
+    })
+}
+
+func LimitBody(next http.Handler, maxBytes int64) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        r.Body = http.MaxBytesReader(w, r.Body, maxBytes)
+        next.ServeHTTP(w, r)
+    })
+}
